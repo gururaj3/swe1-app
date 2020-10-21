1ea77e1187ac92bc6eeec2b7c3b549b876a8ae6e
gururaj3 <guruz.shinde@gmail.com>
2020-10-21 03:23:25 -0400

    fixing bugs

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,7 @@
-from django.shortcuts import render
-
 # from django.template import loader
 # Create your views here.
-from django.http import Http404
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
 from django.utils import timezone
